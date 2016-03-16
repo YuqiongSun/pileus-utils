9a4fb577ca62f48ca24e35732b289d19449d7584
root <root@giga.cse.psu.edu>
2016-03-16 14:55:43 -0400

    update

--- label_file.sh
+++ label_file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dirlist="/lib /lib64 /root/.aptitude .bash_history .bashrc .cache .lesshst .profile .rnd .viminfo"
+dirlist="/lib /lib64 /usr /bin /sbin /etc"
 label="0;|65535;"
 
 if [ $# -eq 1 ];then
@@ -13,6 +13,6 @@ if [ $# -eq 2 ];then
 fi
 
 for d in "$dirlist"; do
-	find $d -exec /usr/bin/setfattr -n security.difc -v "$2" '{}' \; 
+	find $d -exec /usr/bin/setfattr -n security.difc -v "$label" '{}' \; 
 done
 
